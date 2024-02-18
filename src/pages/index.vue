105634da039a95196e3e70ad867d50a8a4b3d779
Vlad Makarov <makvv4@gmail.com>
2024-02-19 05:03:42 +0700

    fix error page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,9 +10,14 @@ const { getDailyRates } = useCurrenciesStore()
     <div v-if="dailyRatesIsFetching" class="flex flex-wrap justify-center gap-3 my-4">
       <Icon icon="svg-spinners:ring-resize" width="40" />
     </div>
-    <div v-else-if="dailyRatesError" class="flex-col flex-wrap justify-center gap-3 my-4">
-      <p>Ошибка</p>
-      <button class="btn" @click="() => getDailyRates()"><Icon icon="mdi:reload" /></button>
+    <div
+      v-else-if="dailyRatesError"
+      class="flex-col flex-wrap justify-center space-y-5 my-4 text-center"
+    >
+      <p class="text-3xl">Ошибка</p>
+      <button class="btn btn-ghost" @click="() => getDailyRates()">
+        <Icon icon="mdi:reload" width="30" />
+      </button>
     </div>
     <div
       v-else-if="filteredCurrencies && Object.keys(filteredCurrencies).length"
